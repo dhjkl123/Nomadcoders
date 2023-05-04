666bba23e6f3a55c204b50a29fb16827d3e3a98d
dhjkl123 <dhjkl123@naver.com>
2023-05-04 19:37:19 +0900

    20230504 ksw dart start!

---
+++ dart/Variables/Late_Variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  late final String name;
+  // 무언가 데이터를 받아온다.
+  //print(name); 할당되지 않아 print error -> 실수를 방지
+  name = 'name'; // 나중에 값을 넣을 수 있음
+
+
+}
---
+++ dart/Variables/The_Var_keyword.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'name'; // 지정한 변수는 변경 불가능
+  name = 'kwonseokwon'; // string
+  
+  // 함수나 메소드 내부에 지역 변수를 선언할 때 사용
+  // class의 속성으로는 직접 지정
+    
+  String name_str = 'name';
+  
+}
